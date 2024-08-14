a33a1dbfb169b288bec96ef4da357dadac39c186
CarinaChen423 <chaselc011@gmail.com>
2024-08-14 22:35:20 +0200

    14/08

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+let firstName = 'Mosh'; //String Literal
+console.log(firstName);
+//Variable:
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number
+//Cannot contain a space or hyphen(-)
+//Are case-sensitive
+let secondName = 'Roy';
+let lastName = 'Jane';
+
+//Constant:
+const interestRate = 0.3;
+console.log(interestRate);
+//Constant type cannot be changed
+
+//Primitive(Value) Types: String Number Boolean undefined null
+//Reference Types
+let age = 30; //Number Literal
+let isApproved = true; //Boolean Literal
+let myName; //undefined
+let userImage = null;
+
+//Dynamic language & Static language
